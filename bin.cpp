c9f6aee7f0936b13c3500b6b5d0d1c2c3881a329
creatordanieleinzerillo <creatordanieleinzerillo@gmail.com>
2024-07-29 01:45:20 +0200

    Now the number of cells is limitless, the only limit is your ram ;)

--- bin.cpp
+++ bin.cpp
@@ -1,13 +1,18 @@
-
-/*
-* Implementare un Garbage Collector appena hai tempo
-*/
 #include <iostream> 
 #include <stdlib.h>
 #include <vector>
 #include <random>
 #include <fstream>
+#include <stack>
 using namespace std;
+
+struct movement {
+    int x;
+    int y;
+    int typeofMov;
+};
+
+typedef struct movement movement;
 
 class Cella{
     public: 
@@ -31,61 +36,76 @@ class labirinto {
     private:
         int n;
 
-        vector<pair<int, int>> takePossibility(int x, int y){
-            vector<pair<int, int>> possibilities;
+        vector<movement> takePossibility(int x, int y){
+            vector<movement> possibilities;
             if(x - 1 >= 0 && maze[x - 1][y].visited == false)
-                possibilities.push_back({x - 1, y});
+                possibilities.push_back((movement){x - 1, y, 1});
             if(y - 1 >= 0 && maze[x][y - 1].visited == false)
-                possibilities.push_back({x, y - 1});
+                possibilities.push_back((movement){x, y - 1, 2});
             if(x + 1 < n && maze[x + 1][y].visited == false)
-                possibilities.push_back({x + 1, y});
+                possibilities.push_back((movement){x + 1, y, 3});
             if(y + 1 < n && maze[x][y + 1].visited == false)
-                possibilities.push_back({x, y + 1});
+                possibilities.push_back((movement){x, y + 1, 4});
             return possibilities;
         }   
 
     public: 
-        vector<vector<Cella>> maze;
+        Cella** maze;
         
         labirinto(int n){
             this -> n = n; 
-            maze.resize(n);
-            fill(maze.begin(), maze.end(), vector<Cella>(n, Cella()));
+            maze = (Cella**) malloc(n * sizeof(Cella*));
+            for(size_t i = 0; i < n; i++){
+                maze[i] = (Cella*) malloc(n * sizeof(Cella));
+                for(size_t j = 0; j < n; j++){
+                    maze[i][j] = Cella();
+                }
+            }
+            //fill(maze.begin(), maze.end(), vector<Cella>(n, Cella()));
         }
 
-        void destroywall(int x, int y, int newX, int newY){
-            if(maze[newX][newY].visited)
-                return;
-            if(newY - y == -1){
-                maze[newX][newY].wallRight = false;
-                maze[x][y].wallLeft = false;
-            }
-            else if((newY - y) == 1){
-                maze[newX][newY].wallLeft = false;
-                maze[x][y].wallRight = false;
-            }
-            else if(newX - x == 1){
-                maze[newX][newY].wallUp = false;
+        void destroywall(int x, int y, int kindOfMovement){
+            if(kindOfMovement == 1){
                 maze[x][y].wallDown = false;
-            } else {
-                maze[newX][newY].wallDown = false;
-                maze[x][y].wallUp = false;
+                maze[x+1][y].wallUp = false;
             }
-            
+            else if(kindOfMovement == 2){
+                maze[x][y].wallRight = false;
+                maze[x][y + 1].wallLeft = false;
+            }
+            else if(kindOfMovement == 3){
+                maze[x][y].wallUp = false;
+                maze[x - 1][y].wallDown = false;
+            }
+            else if(kindOfMovement == 4){
+                maze[x][y].wallLeft = false;
+                maze[x][y - 1].wallRight = false;
+            } 
+            return;
         }
 
         void makemaze(int x, int y){
-            maze[x][y].visited = true;
-            
-            vector<pair<int, int>> possibilities = takePossibility(x, y);
-            if(possibilities.size() == 0)
-                return; 
-            while(possibilities.size() > 0){
-                const int choose = rand() % possibilities.size();                
-                destroywall(x, y, possibilities[choose].first, possibilities[choose].second);
-                makemaze(possibilities[choose].first, possibilities[choose].second);
-                possibilities.erase(possibilities.begin() + choose); 
+            stack<movement> moves;
+            moves.push((movement){x, y, 0});
+            while(!moves.empty()){
+                x = moves.top().x;
+                y = moves.top().y;
+                size_t kindOfMovement = moves.top().typeofMov;
+                moves.pop();
+                if(maze[x][y].visited)
+                    continue;
+                destroywall(x, y, kindOfMovement);
+                maze[x][y].visited = true;
+                vector<movement> possibilities = takePossibility(x, y);
+                if(possibilities.empty())
+                    continue;
+                size_t choose = rand() % possibilities.size();
+                
+                for(size_t i = 0; i < possibilities.size(); i++){
+                    moves.push(possibilities[(choose + i) % possibilities.size()]);
+                }
             }
+
         }
 
         void write(){
@@ -93,11 +113,14 @@ class labirinto {
             ofstream linux_maze_info;
             linux_maze_info.open("source/linux-amd64/MazeInfo.txt");
             
-            for(vector<Cella> riga : maze)
-                for(Cella cella : riga){
+            for(size_t i = 0; i < n; i++){
+                Cella* riga = maze[i];
+                for(size_t j = 0; j < n; ++j){
+                    Cella cella = riga[j];
                     if(!cella.visited) continue;
                     Informations += cella.write();
                 } 
+            }
             linux_maze_info << Informations;
             linux_maze_info.close();
             ofstream testing;
@@ -105,6 +128,13 @@ class labirinto {
             testing << Informations;
             testing.close();
             return;
+        }
+
+        void freeMaze(){
+            for(size_t i = 0; i < n; i++){
+                free(maze[i]);
+            }
+            free(maze);
         }
 };
 
@@ -118,5 +148,6 @@ int main(int argc, char** argv){
     NuovoLabirinto.makemaze(0, 0);
     NuovoLabirinto.write();
     system("./source/linux-amd64/source");
+    NuovoLabirinto.freeMaze();
     return 0; 
 }
